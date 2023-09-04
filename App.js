780fac7227772b6a1e6d81bc02a4efd0dc628b2c
Martin Manelli <manelli.ml@gmail.com>
2023-09-04 02:30:33 -0300

    Use icons in tab bar

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { AnalyticsScreen, ExpenseScreen, MainScreen } from './src/app/screens';
 
 const Tab = createBottomTabNavigator();
@@ -7,7 +8,30 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'gray',
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let iconSize;
+
+            if (route.name === 'Main') {
+              iconName = 'apps'
+            } else if (route.name === 'Expense') {
+              iconName = 'add-circle'
+            } else if (route.name === 'Analytics') {
+              iconName = 'bar-chart'
+            }
+
+            iconName = focused ? iconName : iconName + '-outline';
+            iconSize = focused ? size * 1.5 : size;
+            
+            return <Ionicons name={iconName} size={iconSize} color={color} />;
+          }
+        })}
+      >
         <Tab.Screen name="Analytics" component={AnalyticsScreen} />
         <Tab.Screen name="Expense" component={ExpenseScreen} />
         <Tab.Screen name="Main" component={MainScreen} />
